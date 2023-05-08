3aae03225aa080a55b6ac9d286b1968f5390f99c
naufalrays <rayhannaufalherlano@gmail.com>
2023-05-08 23:29:41 +0700

    Add Navigation view

---
+++ application/views/navbar.php
@@ -0,0 +1,43 @@
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Project Akhir</title>
+     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+     <style>
+         .fit-image {
+             width: 100%;
+             /* display: inline-flex; */
+             /* height: 70px; */
+             /* only if you want fixed height */
+         }
+     </style>
+ </head>
+
+ <body>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+         <div class="container">
+             <a class="navbar-brand" href="#">Sistem Multimedia</a>
+             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                 <span class="navbar-toggler-icon"></span>
+             </button>
+             <div class="collapse navbar-collapse" id="nZavbarSupportedContent">
+                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                     <li class="nav-item">
+                         <a class="nav-link" aria-current="page" href="#">Users</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link" href="#">Create</a>
+                     </li>
+                 </ul>
+             </div>
+         </div>
+     </nav>
+     <!-- </body> -->
+
+     <main class="container">
+
+         <!-- </html> -->
---
+++ application/views/users.php
@@ -0,0 +1,93 @@
+<div class="ms-4 mt-4">
+    <h3>Daftar Mahasiswa</h3>
+    <br>
+    <div class="border rounded-4">
+        <div class="row ms-2">
+            <div class="col-sm-1 align-self-center">
+                <img class="fit-image mt-3 mb-3 center rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="">
+            </div>
+            <!-- <div class="col-sm-9">
+                <div class="row">
+                    <div class="col-8 col-sm-6 align-self-center">
+                        <h5>
+                            Fadiyah Nazihfa Ukhti
+                        </h5>
+                        <p>
+                            52419139
+                        </p>
+                    </div>
+                    <div class="col-4 col-sm-6">
+                        Level 2: .col-4 .col-sm-6
+                    </div>
+                </div>
+            </div> -->
+            <div class="col-sm-9 align-self-center">
+                <h5>
+                    Fadiyah Nazihfa Ukhti
+                </h5>
+                <h8>
+                    55419222
+                </h8>
+            </div>
+            <div class="col-sm-2 align-self-center">
+                <button class="btn btn-primary rounded-3 me-2">Edit</button>
+                <button class="btn btn-danger rounded-3">Delete</button>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="border rounded-4">
+        <div class="row ms-2">
+            <div class="col-sm-1 align-self-center">
+                <img class="fit-image mt-3 mb-3 center rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="">
+            </div>
+            <!-- <div class="col-sm-9">
+                <div class="row">
+                    <div class="col-8 col-sm-6 align-self-center">
+                        <h5>
+                            Fadiyah Nazihfa Ukhti
+                        </h5>
+                        <p>
+                            52419139
+                        </p>
+                    </div>
+                    <div class="col-4 col-sm-6">
+                        Level 2: .col-4 .col-sm-6
+                    </div>
+                </div>
+            </div> -->
+            <div class="col-sm-9 align-self-center">
+                <h5>
+                    Fadiyah Nazihfa Ukhti
+                </h5>
+                <h8>
+                    55419222
+                </h8>
+            </div>
+            <div class="col-sm-2 align-self-center">
+                <button class="btn btn-primary rounded-3 me-2">Edit</button>
+                <button class="btn btn-danger rounded-3">Delete</button>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="border rounded-4">
+        <div class="row ms-2">
+            <div class="col-sm-1 align-self-center">
+                <img class="fit-image mt-3 mb-3 center rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="">
+            </div>
+            <div class="col-sm-9 align-self-center">
+                <h5>
+                    Fadiyah Nazihfa Ukhti
+                </h5>
+                <h8>
+                    55419222
+                </h8>
+            </div>
+            <div class="col-sm-2 align-self-center">
+                <button class="btn btn-primary rounded-3 me-1">Edit</button>
+                <button class="btn btn-danger rounded-3">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
